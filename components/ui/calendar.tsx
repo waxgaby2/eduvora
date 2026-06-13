64e111ab3a8d73e9034efdbc2df070d6fceecc4f
Timothy Abolaji <timothyabolaji414@gmail.com>
2026-06-13 11:11:08 +0100

    Update calendar.tsx
    Bug fix

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -7,7 +7,6 @@ import {
   type DayButton,
   type Locale,
 } from "react-day-picker"
-
 import { cn } from "@/lib/utils"
 import { Button, buttonVariants } from "@/components/ui/button"
 import { ChevronLeftIcon, ChevronRightIcon, ChevronDownIcon } from "lucide-react"
@@ -87,7 +86,6 @@ function Calendar({
             : "flex items-center gap-1 rounded-(--cell-radius) text-sm [&>svg]:size-3.5 [&>svg]:text-muted-foreground",
           defaultClassNames.caption_label
         ),
-        table: "w-full border-collapse",
         weekdays: cn("flex", defaultClassNames.weekdays),
         weekday: cn(
           "flex-1 rounded-(--cell-radius) text-[0.8rem] font-normal text-muted-foreground select-none",
